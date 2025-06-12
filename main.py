f926005567e0e11fee0593364b6b7b31d1061acf
mi-dy <mindaugasdy@gmail.com>
2025-06-12 13:21:36 +0300

    removed hardcoding

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 from stats import count_words, symbol_counter, listify
+import sys
 
 def main():
-    book = "./books/frankenstein.txt"
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book = sys.argv[1]
     book_text = get_book_text(book)
     words_count = count_words(book_text)
     symbol_dict = symbol_counter(book_text)
-    print(f"============ BOOKBOT ============\nAnalyzing book found at books/frankenstein.txt...")
+    print(f"============ BOOKBOT ============\nAnalyzing book found at {sys.argv[1]}...")
     print(f"----------- Word Count ----------\nFound {words_count} total words")
     #for symbol in symbol_dict:
     #    print(f"'{symbol}': {symbol_dict[symbol]}")
